ce0742ba321d15f387ecdf8da53b35e95926ab89
Вигерин Эдуард <vigerin@tochkak.ru>
2022-03-11 13:16:50 +0300

    added comments

--- classes.py
+++ classes.py
@@ -1,14 +1,17 @@
+import logging
 from time import sleep
 
 from loguru import logger
 from typing import List
 
 
-class Credentials:
-    """
+class MigrationErrors(Exception):
+    def __init__(self, *args):
+        logging.error(f'Error occurred during migration: {args}')
 
-    todo add comments
-    """
+
+class Credentials:
+    """Target cloud authorization credentials"""
     def __init__(self, username: str, password: str, domain: str):
         self.username = username
         self.password = password
@@ -16,12 +19,18 @@ class Credentials:
 
 
 class MountPoint:
+    """Mount point object.
+
+    Size of disk vol_size must be set in bytes
+
+    """
     def __init__(self, mount_point: str, vol_size: int):
         self.mount_point = mount_point
         self.vol_size = vol_size
 
 
 class Workload:
+    """Potential machine for migration"""
     def __init__(self, ip: str, credentials: Credentials, storage: List[MountPoint]):
         self.ip = ip
         self.credentials = credentials
@@ -29,6 +38,7 @@ class Workload:
 
 
 class Source:
+    """Source machine parameters"""
 
     def __init__(self, username: str, password: str, ip: str):
         # todo add logging to file via loguru
@@ -42,12 +52,14 @@ class Source:
 
 
 class MigrationTarget:
-    def __init__(
-            self,
-            cloud_type: str,
-            cloud_credentials: Credentials,
-            target_vm: Workload
-    ):
+    """Target for migration.
+
+    cloud_type -- only 'aws', 'azure', 'vsphere', 'vcloud'
+    target_vm -- attributes of target VM in cloud
+
+    """
+    def __init__(self, cloud_type: str,
+                 cloud_credentials: Credentials, target_vm: Workload):
         assert cloud_type in ['aws', 'azure', 'vsphere', 'vcloud'], f'{cloud_type} is not supported'
         self.cloud_type = cloud_type
         self.cloud_credentials = cloud_credentials
@@ -64,8 +76,27 @@ class Migration:
         self.migration_state = migration_state
 
     def run(self):
-        sleep(100)
-        for mount in self.selected_mounts:
-            pass
+        """Process migration
+
+        - copy data only with selected mounts points;
+        - not running migration without 'C:\' mount point
+        """
+
+        self.migration_target.target_vm = self.migration_source
+        self.migration_target.target_vm.storage = self.selected_mounts
+
+        if 'C:\\' in self.migration_target.target_vm.storage:
+            print('Starting migration')
+            try:
+                self.migration_state = 'running'
+                sleep(100)
+                self.migration_state = 'success'
+            except Exception as e:
+                raise MigrationErrors(e)
+        else:
+            print("Mount points list isn't contain system disk. Can't start migration")
+
+    def save_to_file(self):
+        pass
 
 
--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from classes import MigrationTarget
 from classes import Credentials
 from classes import Workload
 from classes import MountPoint
+from classes import Migration
 import os
 
 
 def main(name):
-    mounts = [MountPoint(), MountPoint()]
+    mounts = [MountPoint(MountPoint('h:\\', 120000000), MountPoint('d:\\', 240000000), MountPoint('c:\\', 100000000)), MountPoint()]
     source = Source(os.environ['SOURCE_USER'], os.environ['SOURCE_PASS'], ip='SOURCE_IP')
     credentials = Credentials(os.environ['MIG_USER'], os.environ['MIG_PASS'], os.environ['MIG_DOMAIN'])
     workload = Workload(os.environ['WORKLOAD_IP'], credentials, mounts)
-    migration = MigrationTarget(cloud_credentials=credentials, cloud_type=os.environ['CLOUD_TYPE'], target_vm=workload)
+    target = MigrationTarget(cloud_credentials=credentials, cloud_type=os.environ['CLOUD_TYPE'], target_vm=workload)
+    selected_mounts = [MountPoint('h:\\', 120000000), MountPoint('d:\\', 240000000)]
+    migration = Migration(selected_mounts, workload, target, 'not started')
+    migration.run()
 
 
 if __name__ == '__main__':
